f5d0fb2ed02dcb419dde5833a6a559ce06a43559
dmaiocchi <dmaiocchi@suse.com>
2018-12-09 15:09:02 +0100

    init

--- Main.hs
+++ Main.hs
@@ -1,35 +1,5 @@
-import System.IO (hPutStr, hClose)
-import System.Process.Typed
-import qualified Data.ByteString.Lazy as L
-import qualified Data.ByteString.Lazy.Char8 as L8
-import Control.Concurrent.STM (atomically)
-import Control.Exception (throwIO)
+module Main where
 
-main :: IO ()
-main = do
-    -- Run a process, print its exit code
-    runProcess "true" >>= print
-    runProcess "false" >>= print
+import System.Process
 
-    -- Check that the exit code is a success
-    runProcess_ "true"
-    -- This will throw an exception: runProcess_ "false"
-
-    -- Capture output and error
-    (dateOut, dateErr) <- readProcess_ "date"
-    print (dateOut, dateErr)
-
-    -- Use shell commands
-    (dateOut2, dateErr2) <- readProcess_ "date >&2"
-    print (dateOut2, dateErr2)
-
-    -- Interact with a process
-    let catConfig = setStdin createPipe
-                  $ setStdout byteStringOutput
-                  $ proc "cat" ["/etc/hosts", "-", "/etc/group"]
-    withProcess_ catConfig $ \p -> do
-        hPutStr (getStdin p) "\n\nHELLO\n"
-        hPutStr (getStdin p) "WORLD\n\n\n"
-        hClose (getStdin p)
-
-        atomically (getStdout p) >>= L8.putStr
+main = callCommand "uptime"
